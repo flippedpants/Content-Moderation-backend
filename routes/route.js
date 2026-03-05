8b9a9c67ce6adf9935e80f2528e0db2f3df6d2f6
flippedpants <dakshrandomstuff@gmail.com>
2026-03-05 23:12:26 +0530

    jwt complete

--- routes/route.js
+++ routes/route.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const createAccount = require("../controller/createAccount.js");
 const moderate = require("../controller/moderate.js");
-const verifyApp = require("../middleware/loginAuth.js");
+const verifyApi = require("../middleware/apiAuth.js");
+const login = require("../controller/login.js")
 
 router.post("/register", createAccount);
-router.post("/moderate", verifyApp, moderate);
+router.post("/login", login)
+router.post("/moderate", verifyApi, moderate);
 
 module.exports = router;
